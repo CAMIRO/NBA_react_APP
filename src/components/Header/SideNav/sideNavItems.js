9b333c07895c238f41122c0fd13b2eeb1b23661d
camiro <juancamilosc@hotmail.com>
2019-06-01 21:04:58 -0500

    fixing fontAwesome icons and adding sideNavItems

--- src/components/Header/SideNav/sideNavItems.js
+++ src/components/Header/SideNav/sideNavItems.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 /// styles
-import style from './sideNav.css'
-import FontAwesome from 'react-fontawesome';
+import style from "./sideNav.css";
+///fontAwesome
+import {
+  faHome,
+  faEnvelopeOpenText,
+  faVideo,
+  faSignInAlt,
+  faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const sideNavItems = () => {
-        return (
-            <div className={style.option}>
-                  <Link to="/">
-                    <FontAwesome name="home"/>
-                  </Link>
-            </div>
-        )
-}
-export default sideNavItems
+  const items = [
+    {
+      type: style.option,
+      icon: faHome,
+      text: "Home",
+      link: "/"
+    },
+    {
+      type: style.option,
+      icon: faEnvelopeOpenText,
+      text: "News",
+      link: "/news"
+    },
+    {
+      type: style.option,
+      icon: faVideo,
+      text: "Videos",
+      link: "/videos"
+    },
+    {
+      type: style.option,
+      icon: faSignInAlt,
+      text: "SignIn",
+      link: "/sign-in"
+    },
+    {
+      type: style.option,
+      icon: faSignOutAlt,
+      text: "SignOut",
+      link: "/sign-out"
+    }
+  ];
+  const showItems = () => {
+    return items.map((item, i) => {
+      return (
+        <div className={item.type} key={i}>
+          <Link to={item.link}>
+            <FontAwesomeIcon icon={item.icon} />
+            {item.text}
+          </Link>
+        </div>
+      );
+    });
+  };
+
+  return <div>{showItems()}</div>;
+};
+export default sideNavItems;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import style from './header.css';
-import { Link } from 'react-router-dom';
-import FontAwesome from "react-fontawesome";
+import React from "react";
+import style from "./header.css";
+import { Link } from "react-router-dom";
 
+///fontAwesome
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 //COMPONENTS
-import SideNav from '../Header/SideNav/sideNav';
+import SideNav from "../Header/SideNav/sideNav";
 
 export default function header(props) {
+  const navBars = () => (
+    <FontAwesomeIcon
+      icon={faBars}
+      onClick={props.onOpenNav}
+      style={{
+        color: "#dfdfdf",
+        padding: "10px",
+        cursor: "pointer"
+      }}
+    />
+  );
 
-    const navBars = () => (
-        
-                <FontAwesome 
-                    name="bars"
-                    onClick={props.onOpenNav}
-                    style={{
-                        color:'#dfdfdf',
-                        padding:'10px',
-                        cursor:'pointer'
-                    }}
-                
-                />
-     
-    )
+  const logo = () => (
+    <Link to="/" className={style.logo}>
+      <img alt="NBA logo" src="/images/nba_logo.png" />
+    </Link>
+  );
+  //console.log('props en el header', props)
+  return (
+    <header className={style.header}>
+      <SideNav {...props} />
 
-    const logo = () =>(
-        <Link to="/" className={style.logo}>
-            <img alt="NBA logo" src="/images/nba_logo.png" />
-        </Link>
-    )
-    //console.log('props en el header', props)
-    return (
-        <header className={style.header}>
-
-            <SideNav {...props}/>
-
-             <div className={style.headerOpt}>
-                 {navBars()}
-                 {logo()}
-             </div>
-        </header>
-    )
+      <div className={style.headerOpt}>
+        {navBars()}
+        {logo()}
+      </div>
+    </header>
+  );
 }
