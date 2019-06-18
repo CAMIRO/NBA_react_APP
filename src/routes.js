a47c867dfeb507dc6ec23fe9bc0d295cd161c00d
camiro <juancamilosc@hotmail.com>
2019-06-18 10:19:21 -0500

    adding article component

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
 ////
-import Home from './components/Home/home'
-import Layout from './hoc/Layout/layout';
+import Home from "./components/Home/home";
+import Layout from "./hoc/Layout/layout";
 
+import NewsArticles from "./components/Articles/News/Post/index";
 
 export default class Routes extends Component {
-    render(){
-        return(
-            <Layout>
-                <Switch>
-                    <Route path="/" exact component={Home}/>
-                </Switch>
-            </Layout>
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/articles/:id" exact component={NewsArticles} />
+        </Switch>
+      </Layout>
+    );
+  }
 }
