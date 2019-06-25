18fa481be3c659eec0cdda9dadea7240888ed391
camiro <juancamilosc@hotmail.com>
2019-06-25 09:24:13 -0500

    components team and title added

--- src/components/Articles/News/Post/header.js
+++ src/components/Articles/News/Post/header.js
@@ -1,6 +1,19 @@
 import React from "react";
+import TeamNfo from "../../Elements/teamNfo";
+import PostData from "../../Elements/PostData";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = props => {
+  const teamNfo = team => {
+    return team ? <TeamNfo team={team} /> : null;
+  };
+
+  const postData = (date, author) => <PostData data={{ date, author }} />;
+
+  return (
+    <div>
+      {teamNfo(props.teamData)}
+      {postData(props.date, props.author)}
+    </div>
+  );
 };
 export default Header;
