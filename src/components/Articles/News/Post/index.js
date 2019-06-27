9aacbd20d17ff36cb2c7327d5cc24422d0dc64bc
juan Sarmiento <juan.sarmiento@unsatotomas.edu.co>
2019-06-27 07:44:12 -0500

    body code added

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -7,7 +7,7 @@ import styles from "../../articles.css";
 
 ////COMPONENTS
 import Header from "./header";
-import Body from "./body";
+
 
 export default class NewsArticles extends Component {
   state = {
@@ -16,8 +16,7 @@ export default class NewsArticles extends Component {
   };
 
   componentWillMount() {
-    axios
-      .get(`${URL}/articles?id=${this.props.match.params.id}`)
+    axios.get(`${URL}/articles?id=${this.props.match.params.id}`)
       .then(response => {
         let article = response.data[0];
 
@@ -40,7 +39,20 @@ export default class NewsArticles extends Component {
           date={article.date}
           author={article.author}
         />
-        <Body />
+        {/* body */}
+        <div className={styles.articleBody}>
+          <h1>{article.title}</h1>
+          <div className={styles.articleImage}
+          style={{
+              background:`url('/images/articles/${article.image}')`
+            }}
+          
+          ></div>
+            <div className={styles.articleText}>
+              {article.body}
+            </div>
+        </div>
+      
       </div>
     );
   }
