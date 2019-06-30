0e3c601be3f18a9fded61fd5448ece1c0ee73a0a
camiro <juancamilosc@hotmail.com>
2019-06-30 08:24:04 -0500

    added the videos section

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Home from "./components/Home/home";
 import Layout from "./hoc/Layout/layout";
 
 import NewsArticles from "./components/Articles/News/Post/index";
+import VideoArticles from "./components/Articles/Videos/Video/index";
 
 export default class Routes extends Component {
   render() {
@@ -14,6 +15,7 @@ export default class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/articles/:id" exact component={NewsArticles} />
+          <Route path="/videos/:id" exact component={VideoArticles} />
         </Switch>
       </Layout>
     );
